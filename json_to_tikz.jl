d59564d29f0634293d47fc0dfcf34ab0af29260a
r13ef <73205763+r13ef@users.noreply.github.com>
2024-08-09 12:45:37 +0900

    modify the cordinates of tikz_picture

--- json_to_tikz.jl
+++ json_to_tikz.jl
@@ -1,20 +1,20 @@
 
-function tikz_picture(n::Int64, edges::Vector{Vector{Vector{Int64}}}, consv::Vector{Vector{Int64}})
+using JSON
 
+function tikz_picture(n::Int64, num::Int64, edges::Vector{Vector{Vector{Int64}}}, consv::Vector{Vector{Int64}})
     maximal_size = n - 1
     scale = 0.5
-    get_coordinate(x, y) = (-x + y, x + y + maximal_size)
-    (base_x, base_y) = () 
-
+    get_cordinate(x, y) = (-x + y, x + y + maximal_size)
+    tikz_cordinate(x,y) = ((-x + y) * scale, (x + y + maximal_size) * scale)
     tikz_code = "\\begin{tikzpicture}\n"
-    
+
     for x in 0:maximal_size
-        (tikz_x, tikz_y) = get_coordinate(x,0)
-        tikz_code *= "\\coordinate (x$x) at ($(tikz_x * scale), $(tikz_y * scale)) node at (x$x) [below left] {\$$x\$};\n"
+        (tikz_x, tikz_y) = tikz_cordinate(x,0)
+        tikz_code *= "\\coordinate (x$num-$x) at ($tikz_x, $tikz_y) node at (x$num-$x) [below left] {\$$x\$};\n"
     end
     for y in 0:maximal_size
-        (tikz_x, tikz_y) = get_coordinate(0,y)
-        tikz_code *= "\\coordinate (y$y) at ($(tikz_x * scale), $(tikz_y * scale)) node at (y$y) [below right] {\$$y\$};\n"
+        (tikz_x, tikz_y) = tikz_cordinate(0,y)
+        tikz_code *= "\\coordinate (y$num-$y) at ($tikz_x, $tikz_y) node at (y$num-$y) [below right] {\$$y\$};\n"
     end
 
     consv_tex = ""
@@ -26,51 +26,62 @@ function tikz_picture(n::Int64, edges::Vector{Vector{Vector{Int64}}}, consv::Vec
         end
         consv_tex = "$(consv_tex[1:end-1]),"
     end
-    tikz_code *= "\\coordinate (c) at (0,1) node at (c) {\$$consv_tex\$};\n"
+    tikz_code *= "\\coordinate (c$num) at (0,$(scale * 2)) node at (c$num) {\$$consv_tex\$};\n"
 
     for x in 0:maximal_size
         for y in 0:maximal_size
-            (tikz_x, tikz_y) = get_coordinate(x,y)
-            tikz_code *= "\\fill[gray] ($(tikz_x * scale), $(tikz_y * scale)) circle (2pt);\n"
+            (tikz_x, tikz_y) = tikz_cordinate(x,y)
+            tikz_code *= "\\fill[gray] ($tikz_x, $tikz_y) circle (2pt);\n"
         end
     end
 
-    flag = false
     for edge in edges
         origin = edge[1]
         minimal_vertex = edge[1]
-
-        for target in edge
-            if origin[1] == origin[2] 
+        flag = false
+        if origin[1] == origin[2] 
+            flag = true
+        end
+        for target in edge[2:end]
+            if target[1] == target[2] 
                 flag = true
             end
-            (origin_x, origin_y) = get_coordinate(origin[1], origin[2])
-            (target_x, target_y) = get_coordinate(target[1], target[2])
-            dx = target_x - origin_x
-            dy = target_y - origin_y
+            (ox,oy) = get_cordinate(origin[1], origin[2])
+            (tx, ty) = get_cordinate(target[1], target[2])
+            dx = tx - ox
+            dy = ty - oy
             bend = gcd(dx, dy) == 1 ? 0 : 12
-            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($(origin_x * scale),$(origin_y * scale)) to [bend right = $bend] ($(target_x * scale),$(target_y * scale));\n"
-            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($(-origin_x * scale),$(origin_y * scale)) to [bend left = $bend] ($(-target_x * scale),$(target_y * scale));\n"
+            (origin_x, origin_y) = tikz_cordinate(origin[1], origin[2])
+            (target_x, target_y) = tikz_cordinate(target[1], target[2])
+            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($origin_x, $origin_y) to [bend right = $bend] ($target_x, $target_y);\n"
+            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($(-origin_x), $origin_y) to [bend left = $bend] ($(-target_x), $target_y);\n"
             origin = target
         end
 
         if !flag
-            (origin_x, origin_y) = get_coordinate(minimal_vertex[1], minimal_vertex[2])
-            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($(origin_x * scale),$(origin_y * scale)) to [bend right = $bend] ($(-origin_x * scale),$(origin_y * scale));\n"
+            (origin_x, origin_y) = tikz_cordinate(minimal_vertex[1], minimal_vertex[2])
+            tikz_code *= "\\draw[<->][line width=1][{Latex[width = 2mm]}-{Latex[width = 2mm]}] ($origin_x, $origin_y) to [bend right = 12] ($(-origin_x), $origin_y);\n"
         end
     end  
+    println(num)
+    println(length(tikz_code))
 
     tikz_code *= "\\end{tikzpicture}\n"
-
     return tikz_code;
 end
 
 
 
-edges = [[[0,0],[1,2],[3,4]],[[0,1],[2,3]],[[0,2],[1,4]],[[0,3],[1,1]],[[0,4],[2,2]]]
-consv = [[0,1,-1,0,0],[0,0,0,1,-1]]
-tikz_code = tikz_picture(5,edges,consv)
+inter_list = JSON.parsefile("output/output_size5.json")
+my_tikz_code = ""
 
+for (i,inter) in enumerate(inter_list)
+    if inter["is_irrq"] 
+        my_edges::Vector{Vector{Vector{Int64}}} = inter["edges"]
+        my_consv::Vector{Vector{Int64}} = inter["consv"]
+        global my_tikz_code *= tikz_picture(inter["n"],i,my_edges,my_consv)
+    end
+end
 
 
 tex_code = """
@@ -80,7 +91,7 @@ tex_code = """
 \\usetikzlibrary{arrows.meta}
 \\usetikzlibrary{arrows}
 \\begin{document}
-$tikz_code
+$my_tikz_code
 \\end{document}
 """
 
